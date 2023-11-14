1516fe7ccedd3397c279d24a900fa7553eff771e
Kochkina Ekaterina <Gudrum@yandex.ru>
2023-11-14 20:32:45 +0100

    refactor: рефакторинг часть 14 - 404 исправить

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {Button} from "@ya.praktikum/react-developer-burger-ui-components";
+
 
 export function NotFound() {
+  const navigate = useNavigate();
+  function onClick() {
+    navigate('/', {replace: false});
+  }
 
-return(
+  return (
     <div className="not-found">
-      <div className="image-not"><Link className="text" to={"/"}><p className="text text_type_main-large text_color_primary">ДОМОЙ</p></Link></div>
-
+      <p className="text text_type_main-large text_color_primary">страница 404</p>
+      <div className="image-not">
+        <Button htmlType={"button"} type="primary" size="medium" children={"ДОМОЙ"} key={"home"} onClick={onClick}/>
+      </div>
     </div>
   )
-
-
 }
