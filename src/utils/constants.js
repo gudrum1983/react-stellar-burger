2eb7f4f33449fde54f673040c096acf78472c8fd
Kochkina Ekaterina <Gudrum@yandex.ru>
2023-11-16 18:50:36 +0100

    feat: начать писать WS

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const WebsocketStatus = {
+  CONNECTING: 'CONNECTING...',
+  ONLINE: 'ONLINE',
+  OFFLINE: 'OFFLINE',
+}
+
+export const feedOrdersActionType = {
+  DATA: 'data',
+  INSERT: 'insert',
+  DELETE: 'delete',
+  UPDATE: 'update',
+  MOVE: 'move',
+}
