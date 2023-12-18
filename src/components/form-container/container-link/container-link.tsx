419b2512a22ce033cd2aa6bdd4de2c4b5e787fbf
Kochkina Ekaterina <Gudrum@yandex.ru>
2023-12-18 09:11:17 +0100

    refactor: типизировать container-link.tsx

---
+++ src/components/form-container/container-link/container-link.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./container-link.module.css"
+import {Link} from "react-router-dom";
+import {Text} from "../../typography/text/text";
+import {COLOR_INACTIVE} from "../../../utils/types";
+
+export type TPropsContainerLink = {
+  textDescription:string;
+  textLink:string;
+  to:string;
+}
+
+export function ContainerLink({textDescription, textLink, to}:TPropsContainerLink):JSX.Element {
+
+  return (
+    <div>
+      <Text color={COLOR_INACTIVE}>
+        {textDescription}
+        <span> <Link className={style.link} to={to}>{textLink}</Link></span>
+      </Text>
+    </div>
+  )
+}
